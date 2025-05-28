db39062c3c14235de4436290b2d7de2772817790
Joaquin-B-S <joaquinkappa@hotmail.com>
2025-05-27 23:18:42 -0400

    QuerySelector

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Seleccionar contenido
+
+// Query Selector
+const titulo = document.querySelector("h1");
+titulo.textContent = "Hola desde JavaScript";
+// Agregar clase al elemento
+titulo.classList.add("text-center");
+// Agregar estilo al elemento
+titulo.style.color = "red";
+
+// Mostrar contenido en consola
+console.log(titulo);
